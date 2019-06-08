90e0017b8507594e671c07b83fd13fff4799a266
Abaadir <engabaadir@gmail.com>
2019-06-08 16:57:27 +0300

    feat:function to run query

--- report_engine.py
+++ report_engine.py
@@ -1,2 +1,20 @@
 #!/usr/bin/env python3
 import psycopg2
+
+def run_query(query):
+    """Connects to the database, runs the query passed to it,
+    and returns the results"""
+
+    db = psycopg2.connect(
+        user="postgres",
+        password="Abc123",
+        host="127.0.0.1",
+        port="5432",
+        database="news"
+    )
+
+    cr = db.cursor()
+    cr.execute(query)
+    rows = cr.fetchall()
+    db.close()
+    return rows
